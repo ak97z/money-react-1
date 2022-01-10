eddbe6d3e790916f01355a16723d72de3c689cd6
ak <123@qq.com>
2022-01-10 22:26:21 +0800

    导航栏布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
 import React from 'react';
-import {BrowserRouter as Router, Link, Redirect, Route, Switch} from 'react-router-dom';
+import {HashRouter as Router, Link, Redirect, Route, Switch} from 'react-router-dom';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  border: 1px red solid;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+const Main = styled.div`
+  border: green 1px solid;
+  flex-grow: 1;
+  overflow: auto`;
+const Nav = styled.div`
+  border: blue 1px solid;
+
+  > ul {
+    display: flex;
+
+    > li {
+      width: 33%;
+      text-align: center;
+      padding: 16px;
+    }
+  }
+`;
 
 function App() {
     return (
         <Router>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/money">money-标签</Link>
-                        </li>
-                        <li>
-                            <Link to="/tags">tags-标签</Link>
-                        </li>
-                        <li>
-                            <Link to="/statistics">statistics-标签</Link>
-                        </li>
-                    </ul>
-                </nav>
+            <Wrapper><Main>
                 <Switch>
                     <Redirect exact from="/" to="/money"/>
                     <Route path="/money">
@@ -30,11 +42,26 @@ function App() {
                         <Statistics/>
                     </Route>
                     <Route path="*">
-                        <NoMatch />
+                        <NoMatch/>
                     </Route>
 
-                </Switch>
-            </div>
+                </Switch></Main>
+                <Nav>
+                    <ul>
+                        <li>
+                            <Link to="/money">money-标签</Link>
+                        </li>
+                        <li>
+                            <Link to="/tags">tags-标签</Link>
+                        </li>
+                        <li>
+                            <Link to="/statistics">statistics-标签</Link>
+                        </li>
+                    </ul>
+                </Nav>
+
+
+            </Wrapper>
         </Router>
     );
 }
@@ -50,7 +77,9 @@ function Statistics() {
 function Tags() {
     return <h2>Tags-页面</h2>;
 }
+
 function NoMatch() {
     return <h2>404</h2>;
 }
+
 export default App;
