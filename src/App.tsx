aabc14f13d0d1d324244ff859107d8591b6a8056
ak <123@qq.com>
2022-01-10 21:27:46 +0800

    react-router初始模板

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Link, Redirect, Route, Switch} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/money">money-标签</Link>
+                        </li>
+                        <li>
+                            <Link to="/tags">tags-标签</Link>
+                        </li>
+                        <li>
+                            <Link to="/statistics">statistics-标签</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route path="/money">
+                        <Money/>
+                    </Route>
+                    <Route path="/tags">
+                        <Tags/>
+                    </Route>
+                    <Route path="/statistics">
+                        <Statistics/>
+                    </Route>
+                    <Redirect exact from="/" to="/money"/>
+                </Switch>
+            </div>
+        </Router>
+    );
+}
+
+function Money() {
+    return <h2>Money-页面</h2>;
+}
+
+function Statistics() {
+    return <h2>Statistics-页面</h2>;
+}
+
+function Tags() {
+    return <h2>Tags-页面</h2>;
 }
 
 export default App;
