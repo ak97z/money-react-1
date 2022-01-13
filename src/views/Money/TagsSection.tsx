72495e49a9918ecc6e15a093e272947ca1b58af4
ak <123@qq.com>
2022-01-13 15:14:51 +0800

    将Tag重构为对象,其中的id属性由createId.tsx自动生成

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import React from 'react';
 import {useTags} from '../../useTags';
+import {createId} from '../../lib/createId';
 
 
 const Wrapper = styled.section`
@@ -40,38 +41,40 @@ const Wrapper = styled.section`
   }`;
 
 type Props = {
-    value: string[];
-    onChange: (selected: string[]) => void
+    value: number[];
+    onChange: (selected: number[]) => void
 }
 const TagsSection: React.FC<Props> = (props) => {
     const {tags, setTags} = useTags();
-    const selectedTags = props.value;
+    const selectedTagIds = props.value;
     const onAddTag = () => {
         const tagName = window.prompt('新标签的名称为:');
         if (tagName !== null) {
-            setTags([...tags, tagName]);
+            setTags([...tags, {id:createId(), name: tagName}]);
+            console.log(tagName);
         }
     };
-    const onToggleTag = (tag: string) => {
-        const index = selectedTags.indexOf(tag);
+    const onToggleTag = (tagId: number) => {
+        const index = selectedTagIds.indexOf(tagId);
         if (index >= 0) {
-            props.onChange(selectedTags.filter(t => t !== tag));
+            //如果tag被选中,就复制所有没有被选中的tag,作为新的selectedTag
+            props.onChange(selectedTagIds.filter(t => t !== tagId));
         } else {
-            props.onChange([...selectedTags, tag]);
+            props.onChange([...selectedTagIds, tagId]);
         }
     };
-    const getClass = (tag: string) => {
-        return (selectedTags.indexOf(tag) >= 0 ? 'selected' : ' ');
+    const getClass = (tagId: number) => {
+        return (selectedTagIds.indexOf(tagId) >= 0 ? 'selected' : ' ');
     };
     return (
         <Wrapper>
             <ol>
-                {tags.map(tag => <li key={tag}
+                {tags.map(tag => <li key={tag.id}
                                      onClick={() => {
-                                         onToggleTag(tag);
+                                         onToggleTag(tag.id);
                                      }}
-                                     className={getClass(tag)}
-                >{tag}</li>)}
+                                     className={getClass(tag.id)}
+                >{tag.name}</li>)}
             </ol>
             <button onClick={onAddTag}>新增标签</button>
         </Wrapper>
